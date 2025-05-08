bc2ebb9af4eb1f2f29cc64144889a47c1ff8cbfd
Hagarib <Hagaribrahim2344@gmail.com>
2025-05-08 09:58:00 +0300

    Sama

---
+++ test.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! 2";
+  cout << "Hello World! 4";
+  cout << "Hello World! 5";
+  cout << "Hello World! 5";
+  
+
+  return 0;
+}
